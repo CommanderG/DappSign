d68b03ce17d5432dd987725ad3e6071225aee567
kovtun1 <admin@mac-admin.local>
2016-03-01 01:23:12 +0200

    Updated buttons on Scoreboard scene.

--- DappSign/ScoreboardVC.swift
+++ DappSign/ScoreboardVC.swift
@@ -9,14 +9,25 @@
 import UIKit
 
 class ScoreboardVC: UIViewController {
+    @IBOutlet weak var profileView: UIView!
+    @IBOutlet weak var composeView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.addBordersToView(self.profileView)
+        self.addBordersToView(self.composeView)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    // MARK: - UI
     
-    
+    private func addBordersToView(view: UIView) {
+        view.layer.borderColor = UIColor.whiteColor().CGColor
+        view.layer.borderWidth = 2.0
+        view.layer.cornerRadius = 6.0
+    }
 }
