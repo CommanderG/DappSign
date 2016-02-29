4676cecad7c25fbaa431c6e8fbada6ac1cb87454
kovtun1 <oleksiykovtun@oleksiys-mac-mini.local>
2016-02-29 21:51:31 +0200

    Worked on Scoreboard UI.

---
+++ DappSign/ScoreboardVC.swift
@@ -0,0 +1,22 @@
+//
+//  ScoreboardVC.swift
+//  DappSign
+//
+//  Created by Oleksiy Kovtun on 2/29/16.
+//  Copyright © 2016 DappSign. All rights reserved.
+//
+
+import UIKit
+
+class ScoreboardVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    
+}
