bb96c071e6e3af4a1c489aeebd1d112d01c78654
kovtun1 <oleksiykovtun@oleksiys-mac-mini.local>
2016-03-01 17:56:47 +0200

    Updated scoreboard UI.

--- DappSign/ScoreboardVC.swift
+++ DappSign/ScoreboardVC.swift
@@ -9,14 +9,15 @@
 import UIKit
 
 class ScoreboardVC: UIViewController {
-    @IBOutlet weak var profileView: UIView!
-    @IBOutlet weak var composeView: UIView!
+    @IBOutlet weak var profileButton:           UIButton!
+    @IBOutlet weak var composeButton:           UIButton!
+    @IBOutlet weak var representativeImageView: UIImageView!
+    @IBOutlet weak var representativeNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.addBordersToView(self.profileView)
-        self.addBordersToView(self.composeView)
+        self.initButtons()
+        self.initRepresentativeImageView()
     }
     
     override func didReceiveMemoryWarning() {
@@ -25,9 +26,27 @@ class ScoreboardVC: UIViewController {
     
     // MARK: - UI
     
+    private func initButtons() {
+        self.addBordersToView(self.profileButton)
+        self.addBordersToView(self.composeButton)
+        
+        let cornerRadius: CGFloat = 6.0
+        
+        self.profileButton.layer.cornerRadius = cornerRadius
+        self.composeButton.layer.cornerRadius = cornerRadius
+    }
+    
+    private func initRepresentativeImageView() {
+        self.addBordersToView(self.representativeImageView)
+        
+        let representativeImageViewWidth = CGRectGetWidth(self.representativeImageView.frame)
+        let cornerRadius = representativeImageViewWidth / 2
+        
+        self.representativeImageView.layer.cornerRadius = cornerRadius
+    }
+    
     private func addBordersToView(view: UIView) {
         view.layer.borderColor = UIColor.whiteColor().CGColor
         view.layer.borderWidth = 2.0
-        view.layer.cornerRadius = 6.0
     }
 }
